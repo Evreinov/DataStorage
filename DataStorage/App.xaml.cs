0976d25896c98b4725b31d01ae62e4690e8aede1
Дмитрий Евреинов <d.evreinov@outlook.com>
2023-07-26 16:25:59 +0300

    Сконфигурирован контейнер сервисов

--- DataStorage/App.xaml.cs
+++ DataStorage/App.xaml.cs
@@ -1,11 +1,36 @@
-﻿using System.Windows;
+﻿using System;
+using DataStorage.ViewModels;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace DataStorage
+namespace DataStorage;
+
+public partial class App
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    private static IServiceProvider? _services;
+
+    private static IConfiguration? _configuration;
+
+    public static IServiceProvider Services => _services ??= GetServices().BuildServiceProvider();
+
+    public static IConfiguration Configuration => _configuration ??= Configure().Build();
+
+    private static IServiceCollection GetServices()
     {
+        var services = new ServiceCollection();
+        InitializeServices(services);
+        return services;
+    }
+
+    private static IConfigurationBuilder Configure()
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("");
+        return config;
+    }
+
+    private static void InitializeServices(IServiceCollection services)
+    {
+        services.AddSingleton<MainWindowViewModel>();
     }
 }
