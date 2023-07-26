28d54a283d4e68bf33f60c65682b692d25321f20
Дмитрий Евреинов <d.evreinov@outlook.com>
2023-07-26 15:48:16 +0300

    Создано модель-предсталение главного окна, провека привязок

---
+++ DataStorage/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using DataStorage.ViewModels.Base;
+
+namespace DataStorage.ViewModels;
+
+internal class MainWindowViewModel : ViewModel
+{
+    #region Title : string - Заголовок окна программы
+
+    /// <summary>Заголовок окна программы</summary>
+    private string _Title;
+
+    /// <summary>Заголовок окна программы</summary>
+    public string Title
+    {
+        get => _Title;
+        set => Set(ref _Title, value);
+    }
+
+    #endregion
+
+    public MainWindowViewModel()
+    {
+        Title = "Главное окно программы DataStorage";
+    }
+}
+
