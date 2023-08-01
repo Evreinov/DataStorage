c4703316b17a4776532cd8cd9239a1a95e6d97f0
Дмитрий Евреинов <d.evreinov@outlook.com>
2023-08-01 14:17:16 +0300

    Доработка представления добавлено поведение SelectedItem для TreeView

---
+++ DataStorage/Infrastructure/Behaviors/TreeViewSelectedItemBehavior.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DataStorage.Infrastructure.Behaviors;
+
+internal class TreeViewSelectedItemBehavior : Behavior<TreeView>
+{
+    public static readonly DependencyProperty SelectedItemProperty = 
+        DependencyProperty.Register(nameof(SelectedItem),
+            typeof(object),
+            typeof(TreeViewSelectedItemBehavior),
+            new FrameworkPropertyMetadata(null) { BindsTwoWayByDefault = true });
+
+    public object SelectedItem
+    {
+        get => (object)GetValue(SelectedItemProperty);
+        set => SetValue(SelectedItemProperty, value);
+    }
+
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+        AssociatedObject.SelectedItemChanged += OnTreeViewSelectedItemChanged;
+    }
+
+    protected override void OnDetaching()
+    {
+        base.OnDetaching();
+        if (AssociatedObject != null)
+        {
+            AssociatedObject.SelectedItemChanged -= OnTreeViewSelectedItemChanged;
+        }
+    }
+
+    private void OnTreeViewSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+    {
+        SelectedItem = e.NewValue;
+    }
+}
+
